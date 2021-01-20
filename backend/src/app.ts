919acc2c9ba2a3876fb2fe238b07950f4612c5c3
richard munoz <richosojason@msn.com>
2021-01-20 12:37:32 -0500

    added file paths to app module

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,18 @@
-import 'dotenv/config'
-import cors from 'cors'
-import express from 'express'
-import router from './routes'
+import "dotenv/config";
+import path from "path";
+import cors from "cors";
+import express from "express";
+import router from "./routes";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(router)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-export default app
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
+app.use("/thumb", express.static(path.join(__dirname, "../thumb")));
+
+app.use(router);
+
+export default app;
