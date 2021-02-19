b77d555280b16b7a631912344786e2278dcc7ef4
richard munoz <richosojason@msn.com>
2021-02-19 10:50:42 -0500

    added styles

--- frontend/src/styles.ts
+++ frontend/src/styles.ts
@@ -1,26 +1,29 @@
-import styled from 'styled-components/native'
-import { StatusBar as eStatusBar } from 'expo-status-bar'
+import styled from "styled-components/native";
+import { StatusBar as eStatusBar } from "expo-status-bar";
 
 export const Colors = {
-  backgroundColor: '#EFEDEE',
-  cardColor: '#ffffff',
-  textColor: '#000000',
-  starColor: '#F9E26B',
-  divisorColor: '#9F9F9F'
-}
+  backgroundColor: "#EFEDEE",
+  cardColor: "#ffffff",
+  textColor: "#000000",
+  alertColor: "#ffabab",
+  starColor: "#F9E26B",
+  divisorColor: "#9F9F9F",
+  disabledColor: "#C0C0C0",
+  primaryColor: "#F9E26B",
+};
 
 export const Spacing = {
   padding: 15,
-  extraMargin: 10
-}
+  extraMargin: 10,
+};
 
 export const Container = styled.View`
   flex: 1;
   backgroundColor: ${Colors.backgroundColor};
   alignItems: flex-start;
   justifyContent: center;
-}`
+}`;
 
 export const StatusBar = styled(eStatusBar).attrs({
-  style: 'auto'
-})``
+  style: "auto",
+})``;
