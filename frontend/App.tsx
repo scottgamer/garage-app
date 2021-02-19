bb348023fcc1adcc3049337d3eca99a85109fbdc
richard munoz <richosojason@msn.com>
2021-02-19 10:51:42 -0500

    added statusbar and navigation

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { Container, StatusBar } from './src/styles'
-import { Provider } from 'react-redux'
-import { store } from './src/store'
+import React from "react";
+import { StatusBar } from "./src/styles";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Garage from "./src/screens/Garage";
+import Detail from "./src/screens/Detail";
+import Form from "./src/screens/CarForm";
 
-import Garage from './src/screens/Garage'
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Container>
-        <StatusBar />
-        <Garage />
-      </Container>
+      <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name="Garage"
+            component={Garage}
+          />
+          <Stack.Screen name="Details" component={Detail} />
+          <Stack.Screen name="Form" component={Form} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
-  )
+  );
 }
